384ab8137ef9bbe8ad2866bb9a3df926adb54933
sairam751 <sairam751348@gmail.com>
2024-08-06 22:58:27 +0530

    Maintaing Components in a particular seprate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,9 @@
-import reactImg from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header.jsx';
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
 
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
 
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
 
 // This is called as object destructring
 //{ image, title, description }
