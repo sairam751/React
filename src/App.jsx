d8c54890346a475bc600dc1f3bb58ed012f94c64
sairam751 <sairam751348@gmail.com>
2024-08-11 15:37:25 +0530

    spliting the components to make app.js file smaller

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 import { useState,Fragment} from 'react';
+import CoreConcepts from './components/CoreConcepts.jsx';
+import Examples from './components/Examples.jsx';
 
 //import { EXAMPLES } from './data.js';
 
@@ -32,31 +34,8 @@ console.log("App Component")
     <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-          {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept key={conceptItem.title} {...conceptItem} />
-            ))}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton isSelected = {selectedTopic === 'components'?true:false} onSelect1={() => handleSelect('components')}>Components</TabButton>
-            <TabButton isSelected = {selectedTopic === 'jsx'?true:false} onSelect1={() =>handleSelect('jsx')}>JSX</TabButton>
-            <TabButton isSelected = {selectedTopic === 'props'?true:false} onSelect1={() =>handleSelect('props')}>Props</TabButton>
-            <TabButton isSelected = {selectedTopic === 'state'?true:false} onSelect1={() =>handleSelect('state')}>State</TabButton>
-          </menu>
-          {!selectedTopic ?<p>Please select a topic.</p>:(
-          <div id="tab-content"> 
-             <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>)}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
     </>
     //</Fragment>
